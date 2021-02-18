c7755b603c8e485d19deef1f1f73cfe3ac790c10
T0jgli <kovalikadam00@gmail.com>
2021-02-18 21:19:48 +0100

    fix

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use("/", indexRoute);
 app.use("/api", urlRoute);
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 })
 
 app.listen(PORT, () => {
