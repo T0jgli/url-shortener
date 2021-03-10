aca11b0961e6c11cd3591eb5e98065c97189e8aa
T0jgli <kovalikadam00@gmail.com>
2021-03-10 19:08:27 +0100

    use cors in development mode

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const indexRoute = require("./routes/indexRoute.js");
 const urlRoute = require("./routes/urlRoute.js");
 const dbConnect = require("./db/db.js");
+require("dotenv").config();
 
 dbConnect();
 
@@ -11,6 +13,10 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client", "build")));
+
+if (process.env.NODE_ENV) {
+    app.use(cors())
+}
 
 app.use("/", indexRoute);
 app.use("/api", urlRoute);
